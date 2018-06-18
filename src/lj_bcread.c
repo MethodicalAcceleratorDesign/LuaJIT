e9e790e6bd8251b0d9bc9277eb979cd52a2f388f
afgbloch <bloch.aurelien@gmail.com>
2018-06-18 15:37:57 +0200

    add multiple changes for auditlog

--- src/lj_bcread.c
+++ src/lj_bcread.c
@@ -22,6 +22,7 @@
 #include "lj_bcdump.h"
 #include "lj_state.h"
 #include "lj_strfmt.h"
+#include "lj_auditlog.h"
 
 /* Reuse some lexer fields for our own purposes. */
 #define bcread_flags(ls)	ls->level
@@ -381,6 +382,7 @@ GCproto *lj_bcread_proto(LexState *ls)
     setmref(pt->uvinfo, NULL);
     setmref(pt->varinfo, NULL);
   }
+  lj_auditlog_new_prototype(pt);
   return pt;
 }
 
